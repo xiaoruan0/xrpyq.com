5566cae5d087af508cd4c6e5ecad128469082aaa
xiaoruan0 <88990207+xiaoruan0@users.noreply.github.com>
2022-07-21 15:43:33 +0800

    Add files via upload

---
+++ public/assets/js/layout/error.js
@@ -0,0 +1,135 @@
+(()=>{
+    const app = Vue.createApp({
+        data(){
+            return {
+                article: {},
+                password: {
+                    id: 0, text: null,
+                },
+                hitokoto: {
+                    text: null, load: false,
+                },
+                card: {
+                    tips: 'placard',
+                    title: null,
+                    content: null,
+                },
+            }
+        },
+        mounted(){
+            this.getHitokoto()
+            this.hasArticle()
+            this.hasPlacard()
+            window.onresize = () => {
+                this.scale()
+                this.cardAutoHigth()
+            }
+        },
+        methods: {
+            hasArticle(){
+                const cache = inisHelper.stringfy({api:'article', limit: 8, order: 'is_top desc, views desc'})
+                if (inisHelper.has.session(cache)) {
+                    this.article = inisHelper.get.session(cache)
+                } else this.getArticle()
+            },
+            getArticle(){
+                const cache = inisHelper.stringfy({api:'article', limit: 8, order: 'is_top desc, views desc'})
+                const token = inisHelper.get.cookie('LOGIN-TOKEN')
+                let config  = {}
+                if (!this.empty(token)) config.headers = {'login-token': token}
+                Get('article',{
+                    limit: 8,
+                    order: 'is_top desc, views desc'
+                }, config).then(res=>{
+                    if (res.code == 200) {
+                        this.article = res.data.data
+                        inisHelper.set.session(cache, res.data.data)
+                    }
+                })
+            },
+            // 检查本地缓存
+            hasPlacard(){
+                const cache = inisHelper.stringfy({api:'placard/sql',whereOr:'type,=,web;type,=,all;'})
+                if (inisHelper.has.session(cache)) {
+                    const placard = inisHelper.get.session(cache)
+                    this.card = placard
+                    this.card.tips = 'placard'
+                } else this.getPlacard()
+            },
+            // 获取公告数据
+            getPlacard(){
+                const cahce = inisHelper.stringfy({api:'placard/sql',whereOr:'type,=,web;type,=,all;'})
+                Get('placard/sql',{
+                    whereOr:'type,=,web;type,=,all;',
+                }).then(res=>{
+                    if (res.code == 200 && res.data.count > 0) {
+                        const result = res.data.data[0]
+                        this.card = result
+                        this.card.tips = 'placard'
+                        inisHelper.set.session(cahce, result)
+                    } else this.card.tips = 'site-info'
+                })
+            },
+            // 获取一言数据
+            getHitokoto(){
+                this.hitokoto.load = true
+                Get('file/words').then(res=>{
+                    if (res.code == 200) {
+                        this.hitokoto.text = res.data
+                    }
+                    this.hitokoto.load = false
+                })
+            },
+            // 公告或站长信息自动高度
+            cardAutoHigth(){
+                const markCard = document.querySelector("#home-mark-card .card.first")
+                const error    = document.querySelector("#home-mark-error .card-body")
+                const second   = document.querySelector("#home-mark-card .card.second .card-body")
+                const scroll   = document.querySelector("#home-mark-card .card.second .card-body .customize-scroll")
+                const height = {
+                    markCard:markCard.offsetHeight,
+                    banner:error.offsetHeight,
+                    second:second.offsetHeight,
+                    scroll:scroll.offsetHeight,
+                }
+                scroll.style.height = height.banner - height.markCard - height.second + height.scroll - 24 + 'px'
+            },
+            // 图片等比例缩放
+            scale(){
+                // 比例
+                const scale = 16 / 9
+                const images = document.querySelectorAll(".preview .article .img-cover")
+                images.forEach(item=>{
+                    const height = item.offsetWidth / scale
+                    item.setAttribute('height', height)
+                })
+            },
+            toPasswordArticle(){
+                if (inisHelper.is.empty(this.password.text)) Notify('请输入密码', 'warning')
+                else {
+                    inisHelper.set.cookie('runtime-' + this.password.id, JSON.stringify(this.password.text))
+                    window.location.href = `/article/${this.password.id}.html`
+                }
+            },
+            // 路由跳转
+            toRoute(url, auth = 'anyone', id = 0){
+                if (auth == 'password') {
+                    this.password.id = id
+                    $('#article-auth-password').modal('show')
+                } else window.location.href = url
+            },
+            // 自然时间
+            nature(time = null,type = 5){
+                return inisHelper.time.nature(time,type)
+            },
+            // 格式化数字
+            format: (value = 0) => inisHelper.format.number(value),
+            // 判断是否为空
+            empty: (data = null) => inisHelper.is.empty(data),
+        },
+        updated(){
+            this.scale()
+            this.cardAutoHigth()
+        },
+    }).mount('#app')
+})()
---
+++ public/assets/js/pages/index/article.js
@@ -0,0 +1,193 @@
+(()=>{
+
+    const app = Vue.createApp({
+        data() {
+            return {
+                hitokoto: {
+                    data: {},
+                    load: false
+                },
+                directory: [],
+                runtime: null,
+                base64:{
+                    load: false,
+                }
+            }
+        },
+        components: {
+            'comment-box': components.commentBox(),
+            'comment-article': components.commentArticle(),
+        },
+        mounted() {
+            this.imagesBox()
+            this.runHitokoto()
+            this.getTagAll()
+        },
+        methods: {
+            imagesBox() {
+                // 获取渲染文章下的全部图片
+                const images = document.querySelector(".markdown").getElementsByTagName("img");
+                for (let item of images) {
+                    // 给图片上预览盒子
+                    item.outerHTML = `<a data-fancybox="gallery" href="${item.src}" data-caption="${item.alt}">${item.outerHTML}</a>`
+                }
+            },
+            // 发起一言请求
+            runHitokoto() {
+                this.hitokoto.load = true
+                inisHelper.fetch.get('https://v1.hitokoto.cn').then(res => {
+                    this.hitokoto = {
+                        data: res,
+                        load: false
+                    }
+                })
+            },
+            // 评论完成
+            finish() {
+                const cache = inisHelper.stringfy({api: 'comments', id: this.aid})
+                // 清理评论相关缓存
+                for (let item in sessionStorage.valueOf()) if (item.indexOf(cache) != -1) {
+                    sessionStorage.removeItem(item)
+                }
+                Notify(`评论成功！但需要刷新一下才能看到<a href='${document.URL}' class="ml-1">刷新</a>`, 'success')
+            },
+            // 获取文章内的全部H1-6标签
+            getTagAll() {
+                let map = []
+                // 采用递归调用的方法，比较方便和简单。
+                const fds = node => {
+                    if (node.nodeType === 1) {
+                        // 这里我们用nodeName属性，直接获取节点的节点名称
+                        const tagName = node.nodeName;
+                        if (tagName.slice(0, 1) == "H" && tagName.slice(0, 2) != "HR") {
+                            if (!inisHelper.is.empty(node.textContent)) {
+                                // 给H标签设置唯一的ID
+                                const id = tagName + '-' + (map.length + 1)
+                                node.setAttribute('id', id)
+                                map.push({
+                                    id,
+                                    name: tagName,
+                                    index: parseInt(tagName.slice(1, 2)),
+                                    text: node.textContent
+                                })
+                            }
+                        }
+                    }
+                    // 递归调用 - 获取该元素节点的所有子节点
+                    for (let item of node.childNodes) fds(item)
+                }
+                fds(document.querySelector('.markdown.preview'))
+                if (!inisHelper.is.empty(map)) {
+                    this.directory = inisHelper.array.to.tree(map)
+                }
+            },
+            // 创建海报
+            createPoster(){
+                $('#poster-alert-modal').modal('show')
+                // 清空canvas
+                this.$refs.qrcode.innerHTML = null
+                const qrcode = new QRCode(this.$refs.qrcode, {
+                    text: window.location.href,
+                    width: 70,
+                    height: 70,
+                    colorDark : "#000000",
+                    colorLight : "#ffffff",
+                    correctLevel : QRCode.CorrectLevel.H
+                });
+            },
+            // 创建画布
+            createCanvas(){
+                html2canvas(this.$refs.canvas).then(canvas => {
+                    // let aTag = document.createElement('a')
+                    // aTag.download = this.$refs.cover.title + '.png'
+                    // aTag.href     = canvas.toDataURL("image/png")
+                    // aTag.dataset.downloadurl = [aTag.download, aTag.href].join(':')
+                    // document.body.appendChild(aTag)
+                    // aTag.click()
+                    // document.body.removeChild(aTag)
+                    canvas.style.setProperty('object-fit','cover')
+                    document.querySelector('#poster-alert-modal .modal-content').appendChild(canvas)
+                });
+            },
+            // 判断字符串是否是base64图片
+            isBase64Image(string){
+                const  reg = /^\s*data:([a-z]+\/[a-z0-9-+.]+(;[a-z-]+=[a-z0-9-]+)?)?(;base64)?,([a-z0-9!$&',()*+;=\-._~:@\/?%\s]*?)\s*$/i;
+                return reg.test(string)
+            },
+            // 更新分享的图片
+            uploadChange(){
+                const file = this.$refs.inputFile.files[0]
+                if (file) {
+                    inisHelper.image.base64(file).then(res => {
+                        this.$refs.cover.src = res
+                    })
+                }
+            },
+            // 定位
+            toTarget: id => $("html,body").animate({scrollTop: $(`#${id}`).offset().top - 200}, 1),
+            // 路由跳转
+            toRoute: url => window.location.href = url,
+            // 自然时间
+            nature: (date = null, type = 5) => inisHelper.time.nature(inisHelper.date.to.time(date), type),
+            // 格式化数字
+            format: (value = 0) => inisHelper.format.number(value),
+            // 判断是否为空
+            empty: (data = null) => inisHelper.is.empty(data),
+        },
+        directives: {
+            highlight: {
+                // 在base.js封装了公共方法
+                mounted: (el) => directives.highlight(el)
+            },
+            base64: {
+                // 在base.js封装了公共方法
+                mounted: (el, binding) => {
+                    if (!binding.instance.isBase64Image(el.src)) {
+                        Get('other/base64', {
+                            url: el.src
+                        }).then(res => {
+                            if (res.code == 200) el.src = res.data.base64
+                        })
+                    }
+                },
+            }
+        },
+        computed: {
+            userInfo(){
+                let result = {
+                    data : {},
+                    token: null,
+                    login: false
+                }
+                if (inisHelper.has.cookie('LOGIN-TOKEN')) {
+                    result = {
+                        data : inisHelper.get.session('USER-INFO'),
+                        token: inisHelper.get.cookie('LOGIN-TOKEN'),
+                        login: true
+                    }
+                } else result.login = false
+                return result
+            }
+        },
+    })
+    app.component('tree', {
+        props: ['array'],
+        methods: {
+            // 定位
+            toTarget(id){
+                $("html,body").animate({scrollTop:$(`#${id}`).offset().top-200},1);
+            },
+        },
+        template: `<li v-for="(item, index) in array" :key="index" class="side-nav-item ps-3">
+            <a v-on:click="toTarget(item.id)" data-bs-toggle="collapse" :href="'#' + item.id + '-mark'" aria-expanded="false" :aria-controls="item.id + '-mark'" class="side-nav-link collapsed py-1">
+                <i class="mdi mdi-tag-multiple-outline text-dark"></i>
+                <span class="text-dark"> {{item.text}} </span>
+                <span v-if="item.children" class="menu-arrow text-dark"></span>
+            </a>
+            <div v-if="item.children" class="collapse" :id="item.id + '-mark'">
+                <tree :array="item.children"></tree>
+            </div>
+        </li>`
+    })
+    app.mount('#app')
+})()
